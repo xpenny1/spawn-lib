d501b59f410501aec8c13c9ef2f17dc6ad3d0ca8
Can Ward <can.ward.99@gmail.com>
2025-05-27 12:59:03 +0200

    added first version of the spawn-lib

--- spawn-lib.sh
+++ spawn-lib.sh
@@ -1,1 +1,27 @@
 #!/usr/bin/env bash
+
+initSpawn() {
+    tempdir=$(mktemp -d)
+    trap "rm -r $tempdir" EXIT
+}
+spawn() {
+    mkfifo "$tempdir/in"
+    mkfifo "$tempdir/out"
+    mkfifo "$tempdir/err"
+    (
+        exec 0< "$tempdir/in"
+        exec 1> "$tempdir/out"
+        exec 2> "$tempdir/err"
+        eval "${@:2}"
+    )&
+    local ind outd errd
+    exec {ind}>  "$tempdir/in"
+    exec {outd}< "$tempdir/out"
+    exec {errd}< "$tempdir/err"
+    for d in ind outd errd; do
+        eval "$1""_""$d""=\$$d";
+    done
+    rm "$tempdir/in"
+    rm "$tempdir/out"
+    rm "$tempdir/err"
+}
